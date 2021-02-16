c218aa7804f392cb1e47678963f883d5a32a18f6
Vadim Pletzer <vadim.pletzer@gmail.com>
2021-02-16 20:23:04 +1300

    syntax fix

--- R/ordinalmodels.R
+++ R/ordinalmodels.R
@@ -206,7 +206,7 @@ unpack.parvec <- function(invect, model, submodel, n, p, q, RG, CG=NULL, constra
                       })
            })
 
-    if (length(invect) != nelts) warning(sprintf("invect is TOO LONG (%d), expected (%d). The parameters may have been specified incorrectly. Please double-check invect.", length(invect), nelts)
+    if (length(invect) != nelts) warning(sprintf("invect is TOO LONG (%d), expected (%d). The parameters may have been specified incorrectly. Please double-check invect.", length(invect), nelts))
 
     parlist
 }
